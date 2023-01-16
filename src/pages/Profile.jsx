97483590da9740976d031ecbd1c693df827c52bd
Niloy173 <niloysaha173@gmail.com>
2023-01-16 17:54:34 +0600

    added images uploaded by indiviual user

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,23 +1,56 @@
-import { signOut } from 'firebase/auth';
+import { motion } from 'framer-motion';
+
+
+/* react files, component, context, custom hooks or modules  */
 import React, { useContext } from 'react';
+import { toast, ToastContainer } from "react-toastify";
+import Modal from '../Components/Modal';
 import { AuthContext } from '../context/AuthContext';
-import { auth } from '../Firebase/config';
+import useFireStore from '../hooks/useFireStore';
 
-const Profile = () => {
 
-  const {dispatch,user} = useContext(AuthContext);
-  
-  const handleLogOut = async () => {
-    try {
-      await signOut(auth);
-      dispatch({ type: 'LOGOUT_STATE' });
+/* svg file */
+import Waiting from "../assets/waiting.svg";
 
-    } catch (error) {
-      console.log(error);
-    }
+
+/* firebase file or modules */
+import { deleteDoc, doc } from 'firebase/firestore';
+import { projectFirestore } from '../Firebase/config';
+
+const Profile = ({selected,setSelectedImage}) => {
+
+  const {user} = useContext(AuthContext);
+  const {docs} = useFireStore("images", user.email);
+
+  const errorToastMessage = (message) => {
+    toast.error(message,{
+      position: toast.POSITION.TOP_RIGHT
+    });
   }
 
+  const successToastMessage = () => {
+    toast.success("Deleted Successfully !",{
+      position: toast.POSITION.TOP_RIGHT
+    });
+  }
+  
+
+  const handleDelete = (id) => {
+    // console.log(id)
+    deleteDoc(doc(projectFirestore, "images",id))
+    .then((response) => {
+      console.log('file deleted successfully')
+      successToastMessage()
+    })
+    .catch((error) => {
+      errorToastMessage(error);
+    })
+   
+  }
+  
   return (
+
+    <React.Fragment>
     
     <section className="py-5 text-center container mt-5">
     
@@ -30,14 +63,99 @@ const Profile = () => {
           <img className="w-25 rounded-circle border-2 mb-4 mt-2" src={user.photoURL} alt={user.displayName} />
           <p className="fs-4 mb-2">{user.displayName}</p>
           <p className="text-muted fs-4 mb-2">{user.email}</p>
-
-          <button className="mt-2 p-2" type="button" onClick={handleLogOut}>Logout</button>
         
         </div>
       
       </div>
 
+
     </section>
+
+    <div className="gallery py-5 bg-light">
+      
+        <div className="container">
+        
+          <div className="row">
+
+          {
+            docs.length > 0 ? docs.map((doc) => (
+
+
+          <motion.div key={doc.id} id={doc.id}  className="col-md-6 col-lg-4 position-relative"
+              layout>
+              <motion.img
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ delay: 1}}
+              src={doc.url}
+              className="w-100 rounded-3 shadow-1 mb-4 cover"
+              alt="gallery"
+              />
+  
+  
+              <motion.div 
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ delay: 1}}
+              className="d-flex justify-content-center position-absolute button_div">
+  
+                <button type="button" 
+                onClick={() => setSelectedImage(doc.url)}
+                className="btn btn-primary btn-block mx-2 text-uppercase">
+                full image</button>
+                
+                <button type="button" 
+                // data-bs-toggle="modal"
+                // data-bs-target="#modal"
+                className="btn btn-dark btn-block text-uppercase"
+                onClick={() => handleDelete(doc.id)} 
+                >delete</button>
+
+              
+  
+          </motion.div>
+
+      {/* Modal  */}
+      {/*<div  className="modal"  id="modal" aria-hidden="true">
+      <div className="modal-dialog">
+        <div className="modal-content">
+            <div className="modal-header">
+                <h5 className="modal-title">Photography Album</h5>
+                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+               </div>
+            <div className="modal-body">
+                <p>Are you sure you want to delete this?</p>
+              </div>
+             <div className="modal-footer">
+                <button type="button" className="btn btn-danger text-uppercase" data-bs-dismiss="modal">Cancel</button>
+                <button onClick={() => handleDelete(doc.id)} type="button" className="btn btn-success text-uppercase">Yes</button>
+              </div>
+          </div>
+        </div>
+            </div>*/}
+                
+    {/* Modal  */}
+    
+    </motion.div>
+
+
+    )):
+
+    (<div className="d-sm-flex flex-column-reverse justify-content-center align-items-center g-3">
+          
+                <img className="my-4 loading" src={Waiting} alt="waiting" />
+               
+    </div>)}
+            
+            
+      </div>
+    </div>
+  </div>
+
+  <Modal image={selected} setSelectedImage={setSelectedImage} />
+
+  <ToastContainer/>
+  </React.Fragment>
   )
 }
 
